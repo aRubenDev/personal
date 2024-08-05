ce70ec144e1d6cc2d75e4ca4fd96fda843eaf5e1
Razhgard <aruben.guerra@gmail.com>
2024-08-05 16:08:28 +0200

    other mvp change

--- src/App.jsx
+++ src/App.jsx
@@ -16,7 +16,6 @@ export default function App() {
         <title>aRubenDev | Portfolio</title>
         <meta name="description" content="A portfolio of A. Ruben Guerra" />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
-        <link rel="icon" href="/favicon.ico" />
         <link rel="canonical" href="https://arubendev.com" />
       </Helmet>
       <BrowserRouter>
