aa2014633c8fb45c86e2bb1b9162f12b4be4b992
Razhgard <aruben.guerra@gmail.com>
2024-08-23 10:21:49 +0200

    New skills added

--- src/components/portfolio/Skills.jsx
+++ src/components/portfolio/Skills.jsx
@@ -8,11 +8,14 @@ import {
   TbBrandJavascript,
   TbBrandCSharp,
   TbSql,
+  TbBrandAws,
+  TbBrandGoogle
 } from "react-icons/tb";
+import { DiDotnet, DiGit, DiGithubBadge, DiBootstrap, DiJenkins } from "react-icons/di";
 
 export default function Skills() {
   return (
-    <section className="flex flex-col w-full h-screen p-4 items-center justify-center">
+    <section className="flex flex-col w-full min-h-screen p-4 items-center">
       <div className="flex flex-col text-center items-center">
         <h1 className="relative flex text-5xl font-bold my-5 mb-16 text-center">
           Habilidades
@@ -24,77 +27,121 @@ export default function Skills() {
         </p>
       </div>
 
-      <div className="grid grid-cols-2 gap-4 w-4/6">
+      <div className="grid grid-cols-2 gap-4 w-4/6 grid-rows-2 space-y-4">
         <div className="flex flex-col">
           <h2 className="relative flex text-2xl font-bold mb-5">
             Frameworks Web
           </h2>
           <ul className="flex flex-col gap-2">
             <li className="flex flex-col gap-2 p-4 rounded-lg hover:shadow-lg transition-all ease-in-out">
-              <div className="flex">
-                <TbBrandLaravel className="text-3xl text-[#B2DAB2]" />
-                Laravel
+              <div className="flex items-center justify-between gap-4">
+                <div className="flex items-center gap-2">
+                  <TbBrandLaravel className="text-3xl text-[#B2DAB2]" />
+                  Laravel
+                </div>
+                <span className="inline-flex items-center rounded-md bg-[#B2DAB2] px-2 py-1 text-xs font-medium text-white ring-1 ring-inset ring-green-500/10">
+                  80%
+                </span>
               </div>
               <dl>
-                <div class="w-[80%] bg-gray-200 rounded-full me-2 p-1">
+                <div className="w-[80%] bg-gray-200 rounded-full me-2 p-1">
                   <div
-                    class="bg-[#8DBF8D] h-2.5 rounded"
+                    className="bg-[#8DBF8D] h-2.5 rounded"
                     style={{ width: "80%" }}
                   ></div>
                 </div>
               </dl>
             </li>
             <li className="flex flex-col gap-2 p-4 rounded-lg hover:shadow-lg transition-all ease-in-out">
-              <div className="flex">
-                <TbBrandDrupal className="text-3xl text-[#B2DAB2]" />
-                Drupal
+              <div className="flex items-center justify-between gap-4">
+                <div className="flex items-center gap-2">
+                  <TbBrandDrupal className="text-3xl text-[#B2DAB2]" />
+                  Drupal
+                </div>
+                <span className="inline-flex items-center rounded-md bg-[#B2DAB2] px-2 py-1 text-xs font-medium text-white ring-1 ring-inset ring-green-500/10">
+                  40%
+                </span>
               </div>
               <dl>
-                <div class="w-[80%] bg-gray-200 rounded-full me-2 p-1">
+                <div className="w-[80%] bg-gray-200 rounded-full me-2 p-1">
                   <div
-                    class="bg-[#8DBF8D] h-2.5 rounded"
+                    className="bg-[#8DBF8D] h-2.5 rounded"
                     style={{ width: "40%" }}
                   ></div>
                 </div>
               </dl>
             </li>
             <li className="flex flex-col gap-2 p-4 rounded-lg hover:shadow-lg transition-all ease-in-out">
-              <div className="flex">
-                <TbBrandWordpress className="text-3xl text-[#B2DAB2]" />
-                WordPress
+              <div className="flex items-center justify-between gap-4">
+                <div className="flex items-center gap-2">
+                  <TbBrandWordpress className="text-3xl text-[#B2DAB2]" />
+                  WordPress
+                </div>
+                <span className="inline-flex items-center rounded-md bg-[#B2DAB2] px-2 py-1 text-xs font-medium text-white ring-1 ring-inset ring-green-500/10">
+                  40%
+                </span>
               </div>
               <dl>
-                <div class="w-[80%] bg-gray-200 rounded-full me-2 p-1">
+                <div className="w-[80%] bg-gray-200 rounded-full me-2 p-1">
                   <div
-                    class="bg-[#8DBF8D] h-2.5 rounded"
+                    className="bg-[#8DBF8D] h-2.5 rounded"
                     style={{ width: "40%" }}
                   ></div>
                 </div>
               </dl>
             </li>
             <li className="flex flex-col gap-2 p-4 rounded-lg hover:shadow-lg transition-all ease-in-out">
-              <div className="flex">
-                <TbBrandReact className="text-3xl text-[#B2DAB2]" />
-                ReactJS
+              <div className="flex items-center justify-between gap-4">
+                <div className="flex items-center gap-2">
+                  <TbBrandReact className="text-3xl text-[#B2DAB2]" />
+                  ReactJS
+                </div>
+                <span className="inline-flex items-center rounded-md bg-[#B2DAB2] px-2 py-1 text-xs font-medium text-white ring-1 ring-inset ring-green-500/10">
+                  60%
+                </span>
               </div>
               <dl>
-                <div class="w-[80%] bg-gray-200 rounded-full me-2 p-1">
+                <div className="w-[80%] bg-gray-200 rounded-full me-2 p-1">
                   <div
-                    class="bg-[#8DBF8D] h-2.5 rounded"
+                    className="bg-[#8DBF8D] h-2.5 rounded"
                     style={{ width: "60%" }}
                   ></div>
                 </div>
               </dl>
             </li>
             <li className="flex flex-col gap-2 p-4 rounded-lg hover:shadow-lg transition-all ease-in-out">
-              <div className="flex">
-                <TbBrandNodejs className="text-3xl text-[#B2DAB2]" />
-                NodeJS
+              <div className="flex items-center justify-between gap-4">
+                <div className="flex items-center gap-2">
+                  <TbBrandNodejs className="text-3xl text-[#B2DAB2]" />
+                  NodeJS
+                </div>
+                <span className="inline-flex items-center rounded-md bg-[#B2DAB2] px-2 py-1 text-xs font-medium text-white ring-1 ring-inset ring-green-500/10">
+                  60%
+                </span>
               </div>
               <dl>
-                <div class="w-[80%] bg-gray-200 rounded-full me-2 p-1">
+                <div className="w-[80%] bg-gray-200 rounded-full me-2 p-1">
                   <div
-                    class="bg-[#8DBF8D] h-2.5 rounded"
+                    className="bg-[#8DBF8D] h-2.5 rounded"
+                    style={{ width: "60%" }}
+                  ></div>
+                </div>
+              </dl>
+            </li>
+            <li className="flex flex-col gap-2 p-4 rounded-lg hover:shadow-lg transition-all ease-in-out">
+              <div className="flex items-center justify-between gap-4">
+                <div className="flex items-center gap-2">
+                  <DiDotnet className="text-3xl text-[#B2DAB2]" />
+                  .NET Core
+                </div>
+                <span className="inline-flex items-center rounded-md bg-[#B2DAB2] px-2 py-1 text-xs font-medium text-white ring-1 ring-inset ring-green-500/10">
+                  40%
+                </span>
+              </div>
+              <dl>
+                <div className="w-[80%] bg-gray-200 rounded-full me-2 p-1">
+                  <div
+                    className="bg-[#8DBF8D] h-2.5 rounded"
                     style={{ width: "60%" }}
                   ></div>
                 </div>
@@ -114,9 +161,9 @@ export default function Skills() {
                 PHP
               </div>
               <dl>
-                <div class="w-[80%] bg-gray-200 rounded-full me-2 p-1">
+                <div className="w-[80%] bg-gray-200 rounded-full me-2 p-1">
                   <div
-                    class="bg-[#8DBF8D] h-2.5 rounded"
+                    className="bg-[#8DBF8D] h-2.5 rounded"
                     style={{ width: "80%" }}
                   ></div>
                 </div>
@@ -128,9 +175,9 @@ export default function Skills() {
                 C#
               </div>
               <dl>
-                <div class="w-[80%] bg-gray-200 rounded-full me-2 p-1">
+                <div className="w-[80%] bg-gray-200 rounded-full me-2 p-1">
                   <div
-                    class="bg-[#8DBF8D] h-2.5 rounded"
+                    className="bg-[#8DBF8D] h-2.5 rounded"
                     style={{ width: "40%" }}
                   ></div>
                 </div>
@@ -142,9 +189,9 @@ export default function Skills() {
                 JavaScript
               </div>
               <dl>
-                <div class="w-[80%] bg-gray-200 rounded-full me-2 p-1">
+                <div className="w-[80%] bg-gray-200 rounded-full me-2 p-1">
                   <div
-                    class="bg-[#8DBF8D] h-2.5 rounded"
+                    className="bg-[#8DBF8D] h-2.5 rounded"
                     style={{ width: "60%" }}
                   ></div>
                 </div>
@@ -156,10 +203,149 @@ export default function Skills() {
                 SQL
               </div>
               <dl>
-                <div class="w-[80%] bg-gray-200 rounded-full me-2 p-1">
+                <div className="w-[80%] bg-gray-200 rounded-full me-2 p-1">
                   <div
-                    class="bg-[#8DBF8D] h-2.5 rounded"
+                    className="bg-[#8DBF8D] h-2.5 rounded"
                     style={{ width: "40%" }}
+                  ></div>
+                </div>
+              </dl>
+            </li>
+          </ul>
+        </div>
+
+        <div className="flex flex-col col-span-2">
+          <h2 className="relative flex text-2xl font-bold mb-5">Otros</h2>
+          <ul className="grid grid-cols-2 gap-2">
+            <li className="flex flex-col gap-2 p-4 rounded-lg hover:shadow-lg transition-all ease-in-out">
+              <div className="flex items-center justify-between gap-4">
+                <div className="flex items-center gap-2">
+                  <DiGit className="text-3xl text-[#B2DAB2]" />
+                  Git
+                </div>
+                <span className="inline-flex items-center rounded-md bg-[#B2DAB2] px-2 py-1 text-xs font-medium text-white ring-1 ring-inset ring-green-500/10">
+                  80%
+                </span>
+              </div>
+              <dl>
+                <div className="w-[80%] bg-gray-200 rounded-full me-2 p-1">
+                  <div
+                    className="bg-[#8DBF8D] h-2.5 rounded"
+                    style={{ width: "80%" }}
+                  ></div>
+                </div>
+              </dl>
+            </li>
+            <li className="flex flex-col gap-2 p-4 rounded-lg hover:shadow-lg transition-all ease-in-out">
+              <div className="flex items-center justify-between gap-4">
+                <div className="flex items-center gap-2">
+                  <DiGithubBadge className="text-3xl text-[#B2DAB2]" />
+                  GitHub
+                </div>
+                <span className="inline-flex items-center rounded-md bg-[#B2DAB2] px-2 py-1 text-xs font-medium text-white ring-1 ring-inset ring-green-500/10">
+                  80%
+                </span>
+              </div>
+              <dl>
+                <div className="w-[80%] bg-gray-200 rounded-full me-2 p-1">
+                  <div
+                    className="bg-[#8DBF8D] h-2.5 rounded"
+                    style={{ width: "80%" }}
+                  ></div>
+                </div>
+              </dl>
+            </li>
+            <li className="flex flex-col gap-2 p-4 rounded-lg hover:shadow-lg transition-all ease-in-out">
+              <div className="flex items-center justify-between gap-4">
+                <div className="flex items-center gap-2">
+                  <DiBootstrap className="text-3xl text-[#B2DAB2]" />
+                  Bootstrap
+                </div>
+                <span className="inline-flex items-center rounded-md bg-[#B2DAB2] px-2 py-1 text-xs font-medium text-white ring-1 ring-inset ring-green-500/10">
+                  60%
+                </span>
+              </div>
+              <dl>
+                <div className="w-[80%] bg-gray-200 rounded-full me-2 p-1">
+                  <div
+                    className="bg-[#8DBF8D] h-2.5 rounded"
+                    style={{ width: "60%" }}
+                  ></div>
+                </div>
+              </dl>
+            </li>
+            <li className="flex flex-col gap-2 p-4 rounded-lg hover:shadow-lg transition-all ease-in-out">
+              <div className="flex items-center justify-between gap-4">
+                <div className="flex items-center gap-2">
+                  <TbBrandAws className="text-3xl text-[#B2DAB2]" />
+                  AWS
+                </div>
+                <span className="inline-flex items-center rounded-md bg-[#B2DAB2] px-2 py-1 text-xs font-medium text-white ring-1 ring-inset ring-green-500/10">
+                  40%
+                </span>
+              </div>
+              <dl>
+                <div className="w-[80%] bg-gray-200 rounded-full me-2 p-1">
+                  <div
+                    className="bg-[#8DBF8D] h-2.5 rounded"
+                    style={{ width: "40%" }}
+                  ></div>
+                </div>
+              </dl>
+            </li>
+            <li className="flex flex-col gap-2 p-4 rounded-lg hover:shadow-lg transition-all ease-in-out">
+              <div className="flex items-center justify-between gap-4">
+                <div className="flex items-center gap-2">
+                  <TbBrandGoogle className="text-3xl text-[#B2DAB2]" />
+                  Google Cloud
+                </div>
+                <span className="inline-flex items-center rounded-md bg-[#B2DAB2] px-2 py-1 text-xs font-medium text-white ring-1 ring-inset ring-green-500/10">
+                  60%
+                </span>
+              </div>
+              <dl>
+                <div className="w-[80%] bg-gray-200 rounded-full me-2 p-1">
+                  <div
+                    className="bg-[#8DBF8D] h-2.5 rounded"
+                    style={{ width: "60%" }}
+                  ></div>
+                </div>
+              </dl>
+            </li>
+            <li className="flex flex-col gap-2 p-4 rounded-lg hover:shadow-lg transition-all ease-in-out">
+              <div className="flex items-center justify-between gap-4">
+                <div className="flex items-center gap-2">
+                  <DiJenkins className="text-3xl text-[#B2DAB2]" />
+                  Jenkins
+                </div>
+                <span className="inline-flex items-center rounded-md bg-[#B2DAB2] px-2 py-1 text-xs font-medium text-white ring-1 ring-inset ring-green-500/10">
+                  40%
+                </span>
+              </div>
+              <dl>
+                <div className="w-[80%] bg-gray-200 rounded-full me-2 p-1">
+                  <div
+                    className="bg-[#8DBF8D] h-2.5 rounded"
+                    style={{ width: "40%" }}
+                  ></div>
+                </div>
+              </dl>
+            </li>
+            <li className="flex flex-col gap-2 p-4 rounded-lg hover:shadow-lg transition-all ease-in-out">
+              <div className="flex items-center justify-between gap-4">
+                <div className="flex items-center gap-2">
+                  <TbBrandPhp className="text-3xl text-[#B2DAB2]" />
+                  PHPUnit
+                </div>
+                <span className="inline-flex items-center rounded-md bg-[#B2DAB2] px-2 py-1 text-xs font-medium text-white ring-1 ring-inset ring-green-500/10">
+                  60%
+                </span>
+              </div>
+              <dl>
+                <div className="w-[80%] bg-gray-200 rounded-full me-2 p-1">
+                  <div
+                    className="bg-[#8DBF8D] h-2.5 rounded"
+                    style={{ width: "60%" }}
                   ></div>
                 </div>
               </dl>
